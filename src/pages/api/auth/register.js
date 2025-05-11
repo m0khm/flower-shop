77760c47bf61fb46c5f3ccfdf8fd29143dfeec80
Орел Михаил <77534887+m0khm@users.noreply.github.com>
2025-05-11 15:26:04 +0300

    Create register.js

---
+++ src/pages/api/auth/register.js
@@ -0,0 +1,14 @@
+import { hash } from 'bcryptjs';
+import { prisma } from '@/lib/prisma';
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') return res.status(405).end();
+  const { name, email, password } = req.body;
+  if (await prisma.user.findUnique({ where:{ email } })) {
+    return res.status(400).json({ message: 'Пользователь существует' });
+  }
+  const user = await prisma.user.create({
+    data: { name, email, password: await hash(password, 10) },
+  });
+  res.status(201).json({ id: user.id });
+}
