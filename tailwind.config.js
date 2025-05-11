9b4f3c025ba6a47311ec07a15ccca6edd657c0ef
Орел Михаил <77534887+m0khm@users.noreply.github.com>
2025-05-11 15:52:07 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/pages/**/*.{js,jsx}',
     './src/components/**/*.{js,jsx}',
+    // например, если есть ещё ui-библиотека:
+    './src/components/ui/**/*.{js,jsx}',
   ],
   theme: { extend: {} },
   plugins: [
