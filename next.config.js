c61e20b32ef7b8ad22465aa4d6727fd5a6256f39
Орел Михаил <77534887+m0khm@users.noreply.github.com>
2025-05-11 15:32:20 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
-import path from 'path';
-export default {
+// next.config.js
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   webpack(config) {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
   },
 };
+
+module.exports = nextConfig;
