7f7bf7955b629c6b40258bf8f56604c9f60da267
Орел Михаил <77534887+m0khm@users.noreply.github.com>
2025-05-11 15:05:50 +0300

    Create [...nextauth].js

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,34 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { prisma } from '@/lib/prisma';
+import { compare } from 'bcryptjs';
+
+export default NextAuth({
+  session: { strategy: 'jwt' },
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Пароль', type: 'password' },
+      },
+      async authorize(creds) {
+        const user = await prisma.user.findUnique({ where: { email: creds.email } });
+        if (user && (await compare(creds.password, user.password))) {
+          return { id: user.id, name: user.name, email: user.email, role: user.role };
+        }
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.role = token.role;
+      return session;
+    },
+  },
+});
