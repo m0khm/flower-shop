1062c5a74a6bfb6e58bcd90b4a4cbc0481a4e908
Орел Михаил <77534887+m0khm@users.noreply.github.com>
2025-05-12 18:27:26 +0300

    Update prisma.js

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,3 +1,15 @@
-import { PrismaClient } from '@prisma/client';
-const globalForPrisma = global;
-export default globalForPrisma.prisma ?? (globalForPrisma.prisma = new PrismaClient());
+import { PrismaClient } from '@prisma/client'
+
+let prisma
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  // в режиме dev сохраняем единый экземпляр в глобале, чтобы при “хот-релоаде” не плодить соединения
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
+}
+
+export default prisma   // именно default-экспорт!
