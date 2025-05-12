7f61a2446e75704f16f8b41c63052725c7324db8
Орел Михаил <77534887+m0khm@users.noreply.github.com>
2025-05-12 17:45:45 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx}',
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}', 
-  ],
+  content: ['./src/**/*.{js,jsx}'],
   theme: { extend: {} },
-  plugins: [require('@tailwindcss/forms'), require('tailwindcss-animate')],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwindcss-animate'),
+  ],
 };
